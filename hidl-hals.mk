c186c83729cb2efd2b897a525a5195fd5bf891a5
Danny Baumann <dannybaumann@web.de>
2019-01-22 14:15:40 +0100

    X01BD: Add touchscreen gestures HIDL HAL
    Change-Id: I02380e387f01fe20f984d2e6daa095f2056c03f9

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -110,7 +110,12 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+
 # Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.asus
+
+# Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.x01bd
 
