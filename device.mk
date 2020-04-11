3c6fc60d40659077f9c50e8bfc4db18a6d339be0
Martin <recordyletsplays@gmail.com>
2020-04-11 11:24:32 +0200

    X01BD: Fix Apex

--- device.mk
+++ device.mk
@@ -37,6 +37,10 @@ PRODUCT_CHARACTERISTICS := nosdcard
 # Enforce RRO
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Apex
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
